bdbb31aab1140d4e0c28b1ba3e2565b1cd4affd6
Akinbola Mubarakah Ayomide <108186137+UmmuAbdiLlah2021@users.noreply.github.com>
2025-08-31 12:35:18 +0100

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,1 +1,11 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>This is a simple JSX component.</p>
+      <p><em>I am learning about JSX!</em></p>
+    </div>
+  );
+}
 
+export default WelcomeMessage;
