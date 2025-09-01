61da96ba36edec1a264f65f5c0c4f23f4b746611
Akinbola Mubarakah Ayomide <108186137+UmmuAbdiLlah2021@users.noreply.github.com>
2025-09-01 01:59:55 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -18,28 +18,28 @@ export default App;
 
 
 
-// import React from 'react';
-// import Header from './Header';
-// import MainContent from './MainContent';
-// import Footer from './Footer';
-// import UserProfile from './components/UserProfile';
+import React from 'react';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header />
-//       <MainContent />
-//       <UserProfile 
-//         name="Alice" 
-//         age="25" 
-//         bio="Loves hiking and photography" 
-//       />
-//       <Footer />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+      <Footer />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
